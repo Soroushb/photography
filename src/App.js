c043b9ce95f8392bccd05e66cd2221a1cb947fa0
Soroush Bahrami <sbahrami41@yahoo.com>
2022-10-31 12:23:48 -0400

    Components and images folder added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from "react";
-
+import { Container, AppBar, Typography, Grow, Grid} from '@mui/material';
+import Zoo from './Images/Zoo.jpg'
+import Posts from "./components/posts/Posts";
+import Form from "./components/forms/Form";
 const App = () => {
 
     return(
-        <>
-        <h1>
-            App
-        </h1>
-        </>
+        
+        <Container maxWidth='lg'>
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={Zoo} alt="memories" height="500"/>
+            </AppBar>
+
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
---
+++ src/components/posts/Posts.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Post from "./post/Post";
+
+const Posts = () => {
+    return(
+        <>      
+        <h1>
+            Posts   
+        </h1>
+        <Post/>
+        <Post/>
+        </>
+
+    )
+}
+
+export default Posts
