143006e2f2fe21e54fb36c43bd0e000e5c077266
Soroush Bahrami <sbahrami41@yahoo.com>
2022-11-04 17:51:59 -0400

    minor changes to css

--- src/components/posts/post/Post.js
+++ src/components/posts/post/Post.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardActions, CardContent, CardMedia, Button, Typography} from "@mui/material"
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, Tab} from "@mui/material"
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import DeleteIcon from '@mui/icons-material/Delete';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
@@ -11,29 +11,46 @@ const Post = ({post, setCurrentId}) => {
 
     const dispatch = useDispatch();
 
+    
+
     return(
         
-        <Card sx={{display: 'flex',
+        <Card 
+        className="postCard"
+        sx={{display: 'flex',
         flexDirection: 'column',
         justifyContent: 'space-between',
-        borderRadius: '15px',
+        borderRadius: '10px',
         height: '100%',
         position: 'relative',}}>
 
+        <a target="_blank" href={post.selectedFile}>
         <CardMedia sx={{height: 0,
         paddingTop: '56.25%',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        backgroundBlendMode: 'darken',}} image={post.selectedFile} title={post.title}/>
+        //backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        //backgroundBlendMode: 'darken',
+        }}
+         image={post.selectedFile} title={post.title}
+         
+         //onClick = { () => {  Tab.open(post.selectedFile,'Image','width=largeImage.stylewidth,height=largeImage.style.height,resizable=1');}}
+         />
+        </a>
+        
+        
 
-        <div sx={{position: 'absolute',
+        <div 
+        className="postCard"
+        sx={{position: 'absolute',
                   top: '20px',
                   left: '20px',
                   color: 'white',}}>
         
-        <Typography variant="h6">{post.creator}</Typography>
-        <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+        <Typography className="cardText" variant="h6">{post.creator}</Typography>
+        <Typography className="cardText" variant="body2">{moment(post.createdAt).fromNow()}</Typography>
         </div>
-        <div sx={{position: 'absolute',
+        <div 
+            className="postCard"
+            sx={{position: 'absolute',
              top: '20px',
              right: '20px',
              color: 'white',}}>
@@ -54,11 +71,10 @@ const Post = ({post, setCurrentId}) => {
         <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => ` #${tag}`)}</Typography>
         </div>
         <Typography sx={{padding: '0 16px'}} variant="h5" gutterBottom>{post.title}</Typography>
-        <CardContent>
-        <Typography sx={{padding: '0 10px'}} variant="body2" color="textSecondary" component="p" >{post.message}</Typography>
-        </CardContent>
+    
 
-        <CardActions sx={{padding: '0 16px 8px 16px',
+        <CardActions      className="postCard"
+                          sx={{padding: '0 16px 8px 16px',
                           display: 'flex',
                           justifyContent: 'space-between',}}>
         <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
