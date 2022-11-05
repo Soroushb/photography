0d1f20464f8392078dc63c04c9d3a02ffaf182b8
Soroush Bahrami <sbahrami41@yahoo.com>
2022-11-04 22:32:25 -0400

    Minor changes to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -119,7 +119,7 @@ function ResponsiveAppBar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Soroush Bahrami" src="/images/Soroush2" />
               </IconButton>
             </Tooltip>
             <Menu
