e67eeb49ce676bb07a53f3f523f908b89a8bff70
Soroush Bahrami <sbahrami41@yahoo.com>
2022-12-03 19:29:31 -0500

    App.js updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React,  { useState, useEffect}  from 'react'
-import { Container, Grow, Grid} from '@mui/material';
+import { Container, Grow, Grid, Paper} from '@mui/material';
 import { getPosts } from '../../actions/posts'
 import { useDispatch} from 'react-redux'
 
 import Posts from '../posts/Posts';
 import Form from '../forms/Form';
+import Pagination from '../Pagination'
 
 const Home = () => {
 
@@ -24,6 +25,9 @@ const Home = () => {
 
         <Grid item xs={12} sm={3}>
                 <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                <Paper elevation={6}>
+                    <Pagination />
+                </Paper>
             </Grid>
             <Grid item xs={12} sm={9}>
                 <Posts setCurrentId={setCurrentId}/>
---
+++ src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostDetails = () => {
+
+    console.log("post Details")
+  return (
+    <div>PostDetails</div>
+  )
+}
+
+export default PostDetails
