bc9733f473a82a0951d0fc52c031273ca51b14d8
Soroush Bahrami <sbahrami41@yahoo.com>
2023-01-24 12:57:32 -0500

    User photos show up on profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,53 +4,38 @@ import { AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Avatar, 
 import {Link, useLocation} from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux'
+import Post from "../posts/post/Post";
+import { Grid, CircularProgress } from "@mui/material"
+
 
 const Profile = () => {
 
     const location = useLocation();
     const [isLoading, setIsLoading] = useState(false)
-
-
-    useEffect(() => {
-        const token = user?.token;
-    
-        if(token){
-          const decodedToken = decode(token)
-          
-          if(decodedToken.exp * 1000 < new Date().getTime()){
-            logout()
-          }
-        }
-    
-        setUser(JSON.parse(localStorage.getItem('profile')))
-      }, [location])
-
-      const logout = () => {
-        setIsLoading(true)
-        dispatch({type: 'LOGOUT'})
-        navigate('/')
-        setUser(null)
-        setTimeout(
-          function() {
-            window.location.reload(false);
-            setIsLoading(false)
-          }, 3000);
-      }
-    
-      const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const posts = useSelector((state) => state.posts)
+    const user = JSON.parse(localStorage.getItem('profile'))
       console.log(user);
     
       const dispatch = useDispatch();
       const navigate = useNavigate();
-    
+      
   return (
-    <div>
-        <Box align="center">
-        <Avatar sx={{ bgcolor: "#FF533D", width: 80, height: 80  }}>{user.result.name.charAt(0)}</Avatar>
-        </Box>
-   
-    </div>
+ 
+    !posts.length ? <CircularProgress/> : (
+      <Grid sx={{display: 'flex', alignItems: 'center',}} container direction="reverse" alignItems="stretch" spacing={3}>
+          {
+          
+              posts.slice(0).reverse().filter((post) => post.creator === user?.result?._id).map((post) => (
+                  <Grid item key={post._id} xs={12} sm={4}>
+                      <Post post={post}/>
+                  </Grid>
+              ))
+          }
+      </Grid>
+  
   )
+)
 }
 
 export default Profile
